7bb568ecd91bfc56b63412d1ab1fff7da1bdf506
bart <bartbien88@gmail.com>
2015-01-18 00:20:27 +0100

    Chart chartjs.org bugfix - chainging db data from string to int

--- sql/own-server/employee.sql
+++ sql/own-server/employee.sql
@@ -10,42 +10,42 @@ CREATE TABLE employees(
 /*---------------------------------------------------------*/
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Ramos', 'Angelica', 'Chief Executive Officer (CEO)', 'London', '2014-06-08', '$1,200,000');
+VALUES (NULL, 'Ramos', 'Angelica', 'Chief Executive Officer (CEO)', 'London', '2014-06-08', '1200000');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Satou', 'Airi', 'Accountant', 'Tokyo', '2013-06-08', '$162,700');
+VALUES (NULL, 'Satou', 'Airi', 'Accountant', 'Tokyo', '2013-06-08', '162700');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Cox', 'Ashton', 'Junior Technical Author', 'San Francisco', '2013-02-08', '$86,000');
+VALUES (NULL, 'Cox', 'Ashton', 'Junior Technical Author', 'San Francisco', '2013-02-08', '86000');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Greer', 'Bradley', 'Software Engineer', 'London', '2013-02-08', '$132,000');
+VALUES (NULL, 'Greer', 'Bradley', 'Software Engineer', 'London', '2013-02-08', '132000');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Wagner', 'Brenden', 'Software Engineer', 'San Francisco', '2012-02-08', '$106,000');
+VALUES (NULL, 'Wagner', 'Brenden', 'Software Engineer', 'San Francisco', '2012-02-08', '106000');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Williamson', 'Brielle', 'Integration Specialist', 'New York', '2011-02-08', '$136,000');
+VALUES (NULL, 'Williamson', 'Brielle', 'Integration Specialist', 'New York', '2011-02-08', '136000');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Nash', 'Bruno', 'Software Engineer', 'London', '2013-07-08', '$101,000');
+VALUES (NULL, 'Nash', 'Bruno', 'Software Engineer', 'London', '2013-07-08', '101000');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Vance', 'Caesar', 'Pre-Sales Support', 'New York', '2013-02-08', '$96,000');
+VALUES (NULL, 'Vance', 'Caesar', 'Pre-Sales Support', 'New York', '2013-02-08', '96000');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Stevens', 'Cara', 'Sales Assistant', 'New York', '2013-02-08', '$96,000');
+VALUES (NULL, 'Stevens', 'Cara', 'Sales Assistant', 'New York', '2013-02-08', '96000');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'Kelly', 'Cedric', 'Senior Javascript  Developer', 'Edinburgh', '2013-02-08', '$96,000');
+VALUES (NULL, 'Kelly', 'Cedric', 'Senior Javascript  Developer', 'Edinburgh', '2013-02-08', '96000');
 
 
 /*---------------------------------------------------------*/
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'testLast', 'testFirst', 'testPosition', 'testOffice', CURRENT_TIMESTAMP, '$100,000');
+VALUES (NULL, 'testLast', 'testFirst', 'testPosition', 'testOffice', CURRENT_TIMESTAMP, '100000');
 
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
-VALUES (NULL, 'superName', 'superF', 'position', 'office', CURRENT_TIMESTAMP, '$120,000');
+VALUES (NULL, 'superName', 'superF', 'position', 'office', CURRENT_TIMESTAMP, '120000');
 
 /*---------------------------------------------------------*/
 INSERT INTO `employees` (`id`, `last_name`, `first_name`, `position`, `office`, `start_date`, `salary`) 
@@ -54,7 +54,7 @@ VALUES (NULL, 'eeee?', 'cio', 'cio', 'cio', NOW(), '2');
 /*-------All this update make the same result-------------*/
 UPDATE  `employees` SET position =  'testPosition :)' WHERE id =  '22';
 UPDATE employees SET position =  'testPosition :)' WHERE id =  '21';
-UPDATE  `employees` SET  `salary` =  '$100,000' WHERE  `employees`.`id` =22;
+UPDATE  `employees` SET  `salary` =  '100,000' WHERE  `employees`.`id` =22;
 
 
 table employee
